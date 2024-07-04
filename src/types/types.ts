40f759d223c6c325fff71af8bb4fbdcaf44ea21f
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-07-04 19:49:03 +0200

    UPD

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,11 @@ export type NavLink = {
     title: string;
     path: string;
 }
+
+export type Movie = {
+    id: number;
+    title: string;
+    backdrop_path: string;
+    poster_path: string;
+    overview: string;
+}
