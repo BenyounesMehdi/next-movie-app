eb00849f5f9f82b331abfbf623f31599869d9957
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-07-05 16:21:40 +0200

    UPD

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,4 +1,4 @@
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+export const API_KEY = process.env.API_KEY
 export const BASE_URL = "https://api.themoviedb.org/3"
 // export const IMAGES_URL = "https://image.tmdb.org/t/p//w500"
 export const IMAGES_URL = "https://image.tmdb.org/t/p/original"
