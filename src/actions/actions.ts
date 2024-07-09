1caa022553a58af993141ae0f67bb875f32a657c
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-07-09 15:11:22 +0100

    ADD fetchData Function

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -11,8 +11,21 @@ export const getMoviesGenres = async () => {
          }
     );
     if (!res.ok) {
-        console.log("error from actions: ")
-        throw new Error('Failed to fetch');
+        throw new Error('Failed to fetch data');
     }
     return res.json();
 }
+
+export const fetchData = async (genre: string, page: number, limit: number) => {
+
+    const moviesByGenreUrl = `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genre}`;
+    const paginatedUrl = `${moviesByGenreUrl}&page=${page}&limit=${limit}`;
+
+    const res = await fetch(paginatedUrl);
+    if (!res.ok) {
+        console.error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+        throw new Error('Failed to fetch data');
+    }
+    return res.json();
+};
+
