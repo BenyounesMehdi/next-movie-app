cd406c6d8f4a3b907cf58abeef9c41ca38535f70
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-07-09 15:12:40 +0100

    ADD The onClick Event

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,11 +1,13 @@
 
 type ButtonProps = {
-    title: string
+    title: string;
+    onClick: () => void
 }
 
-export default function Button ({title}: ButtonProps) {
+export default function Button ({title,  onClick}: ButtonProps) {
     return (
         <button
+            onClick={onClick}
             className="bg-red-500 border-2 border-white font-semibold text-xl rounded-full px-5 py-1  hover:bg-red-700 hover:shadow-lg">
             {title}
         </button>
